38323ae112ca2db1e15e799cc925b9c0b7db06ae
devCarlosAlexandre <carloalexandresousa@gmail.com>
2023-07-14 15:07:35 -0300

    feat: :sparkles: tratamento de erros com middlewares

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/db_connect.js";
 import routes from "./routes/index.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
 
 db.on("error", console.log.bind(console, "Error de conexão"));
 db.once("open", () => {
@@ -11,4 +12,7 @@ const app = express();
 app.use(express.json());
 routes(app);
 
-export default app;
+// eslint-disable-next-line no-unused-vars
+app.use(errorMiddleware);
+
+export default app; 
